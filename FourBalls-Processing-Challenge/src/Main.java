17588d142557d3f0182b1dfd36e18194d69bc948
Chetan Raj <91714355+Beniwal43@users.noreply.github.com>
2024-12-25 13:06:49 +0530

    4 balls prblm

---
+++ FourBalls-Processing-Challenge/src/Main.java
@@ -0,0 +1,27 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public static final int HEIGHT = 480;
+    public static final int WIDTH = 640;
+    public static final int DIAMETER = 10;
+
+    public static void main(String[] args) {
+        PApplet.main("Main", args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+    @Override    public void draw() {
+        background(255);
+        drawCircle();
+    }
+
+    private void drawCircle() {
+        ellipse(mouseX , HEIGHT/2, DIAMETER, DIAMETER);
+    }
+}
