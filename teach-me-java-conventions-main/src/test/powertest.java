2bcf5f2d6c8a5f1f9df45b6b20b8e442b08ee3a6
Chetan Raj <91714355+Beniwal43@users.noreply.github.com>
2024-12-25 19:55:14 +0530

    1

---
+++ teach-me-java-conventions-main/src/test/powertest.java
@@ -0,0 +1,26 @@
+package powerpackage;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PowerFinderTest {
+    @Test
+    public void testOneRaisedToOneIsOne() {
+        assertEquals(1, PowerFinder.calculatePower(1, 1));
+    }
+
+    @Test
+    public void testTwoRaisedToOneIsTwo() {
+        assertEquals(2, PowerFinder.calculatePower(2, 1));
+    }
+
+    @Test
+    public void testTwoPowerTwoIsFour() {
+        assertEquals(4, PowerFinder.calculatePower(2, 2));
+    }
+
+    @Test
+    public void testThreePowerTwoIsNine() {
+        assertEquals(9, PowerFinder.calculatePower(3, 2));
+    }
+}
